f88141425a830aec2d64cbde72cd181ed3f3f6b0
Tejas Rao <nottejasrao@gmail.com>
2018-02-17 16:50:12 -0800

    Add basic gameworld

---
+++ Assets/Scripts/MeshCollisionAdd.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshCollisionAdd : MonoBehaviour
+{
+
+    void Start()
+    {
+        foreach (Transform currentTransform in transform)
+        {
+            currentTransform.gameObject.AddComponent<MeshCollider>().convex = true;
+        }
+    }
+
+}
+
