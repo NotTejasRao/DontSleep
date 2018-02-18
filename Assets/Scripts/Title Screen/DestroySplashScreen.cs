441e92c83df10d43a6581f04d49c51470a261014
Andrew Peterson <andpeterson.ap@gmail.com>
2018-02-17 18:05:31 -0800

    MOOOOOOOOOO TITAL SCREAN BATCH~~!!!
    Hell yah

---
+++ Assets/Scripts/Title Screen/DestroySplashScreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+// This example shows how you could draw the splash screen at the start of a scene. This is a good way to integrate the splash screen with your own or add extras such as Audio.
+public class DestroySplashScreen : MonoBehaviour
+{
+	public float delaytime = 8.77f;
+
+	IEnumerator Start(){
+		yield return new WaitForSeconds (delaytime);
+		GameObject menu = GameObject.Find("Menu");
+		menu.transform.Translate (0, -108, 0);
+	}
+}
