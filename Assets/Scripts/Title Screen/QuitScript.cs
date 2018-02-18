facb7ca814c78c77fbc2148f137453e0cd3bf09c
Andrew Peterson <andpeterson.ap@gmail.com>
2018-02-17 22:39:47 -0800

    Sounds Stuff on Title

---
+++ Assets/Scripts/Title Screen/QuitScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitScript : MonoBehaviour {
+	bool hovering = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) && hovering) {
+			AudioSource audio = GetComponent<AudioSource> ();
+			audio.Play ();
+			Application.Quit();
+		}
+	}
+
+	void OnMouseOver(){
+		hovering = true;
+	}
+	void OnMouseExit(){
+		hovering = false;
+	}
+}
