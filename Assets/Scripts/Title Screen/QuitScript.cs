4d0675047d00113097358b9033e393e93c6ccab8
Andrew Peterson <andpeterson.ap@gmail.com>
2018-02-17 23:23:15 -0800

    Summary

--- Assets/Scripts/Title Screen/QuitScript.cs
+++ Assets/Scripts/Title Screen/QuitScript.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class QuitScript : MonoBehaviour {
 	bool hovering = false;
+	public int delaytime = 1;
 	// Use this for initialization
 	void Start () {
 		
 	}
-
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown (0) && hovering) {
-			AudioSource audio = GetComponent<AudioSource> ();
-			audio.Play ();
 			Application.Quit();
 		}
 	}
-
+	void OnMouseEnter(){
+		transform.localScale += new Vector3 (0.05F, 0, 0.05F);
+		AudioSource audio = GetComponent<AudioSource> ();
+		audio.Play ();
+	}
 	void OnMouseOver(){
+		
 		hovering = true;
 	}
 	void OnMouseExit(){
+		transform.localScale -= new Vector3 (0.05F, 0, 0.05F);
 		hovering = false;
 	}
 }
--- Assets/Scripts/Title Screen/StartScript.cs
+++ Assets/Scripts/Title Screen/StartScript.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class StartScript : MonoBehaviour {
 	bool hovering = false;
+	public int delaytime = 1;
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown (0) && hovering) {
 			Application.LoadLevel(1);
 		}
 	}
-
+	void OnMouseEnter(){
+		transform.localScale += new Vector3 (0.05F, 0, 0.05F);
+		AudioSource audio = GetComponent<AudioSource> ();
+		audio.Play ();
+	}
 	void OnMouseOver(){
 		hovering = true;
 	}
 	void OnMouseExit(){
+		transform.localScale -= new Vector3 (0.05F, 0, 0.05F);
 		hovering = false;
 	}
 }
